67c31684c66bf3b9ad06739606e74a2c2c7a0b27
20103 <ahmed.hamed@shgardi.app>
2022-01-12 16:56:09 +0200

    hello

---
+++ src/main/java/PartnerPortal/US05/TestCases/TC01.java
@@ -0,0 +1,57 @@
+package PartnerPortal.US05.TestCases;
+
+import Framework.Web.TestBaseWeb;
+import PartnerPortal.US01.ViewModel.LoginWebPageView;
+import PartnerPortal.US03.ViewModel.SizeTypeWebPageView;
+import PartnerPortal.US04.ViewModel.SizeWebPageView;
+import PartnerPortal.US05.ViewModel.ProductWebPageView;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TC01 extends TestBaseWeb {
+
+    ProductWebPageView ProductPage;
+
+
+    @Test(priority = 1)
+    public void HappyProduct() throws InterruptedException {
+
+        driver.navigate().to("https://partner-dev.shgardi.app/auth/login");
+        ProductPage = new ProductWebPageView(driver);
+        ProductPage.user_name("subway@gmail.com");
+        ProductPage.pass_word("100100100");
+        ProductPage.log_in();
+        ProductPage.cata_log();
+        ProductPage.pro_duct();
+        ProductPage.create_product();
+        ProductPage.co_de("5");
+        ProductPage.english_name("burger");
+        ProductPage.arabic_name("برجر");
+        ProductPage.english_description("delicious taste");
+        ProductPage.arabic_description("مذاق رائع");
+        ProductPage.ordering("55");
+        ProductPage.select_size_type();
+        ProductPage.size_type();
+        ProductPage.select_size();
+        ProductPage.si_ze();
+        //ProductPage.select_tag();
+        // ProductPage.t_ag();
+        ProductPage.select_category();
+        ProductPage.cat_egory();
+        ProductPage.unitprice("30");
+        ProductPage.productimage();
+        ProductPage.buttonsave(driver);
+        ProductPage.delete();
+        ProductPage.enter();
+        ProductPage.enter();
+
+
+
+
+
+
+    }
+
+
+
+}
